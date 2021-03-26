eda7fca75b62592ee78b0374e9831c2c40a3e717
Patrick-Brennan <patrickb2@hotmail.ca>
2021-03-26 18:39:43 -0400

    Session 3

--- validation/Controls/SelectTableList.ascx
+++ validation/Controls/SelectTableList.ascx
@@ -8,10 +8,10 @@
 
                 </asp:ListBox></td>
             <td>
-                <asp:Button ID="btnAllRight" runat="server" Text="&gt;&gt;" Width="50px"  /><br />
-                <asp:Button ID="btnSelectedRight" runat="server" Text="&gt; " Width="50px"  /><br />
-                <asp:Button ID="btnSelectedLeft" runat="server" Text="&lt; " Width="50px"  /><br />
-                <asp:Button ID="btnAllLeft" runat="server" Text="&lt;&lt;" Width="50px"  />
+                <asp:Button ID="btnAllRight" runat="server" Text="&gt;&gt;" Width="50px" OnClick="btnAllRight_Click"  /><br />
+                <asp:Button ID="btnSelectedRight" runat="server" Text="&gt; " Width="50px" OnClick="btnSelectedRight_Click"  /><br />
+                <asp:Button ID="btnSelectedLeft" runat="server" Text="&lt; " Width="50px" OnClick="btnSelectedLeft_Click"  /><br />
+                <asp:Button ID="btnAllLeft" runat="server" Text="&lt;&lt;" Width="50px" OnClick="btnAllLeft_Click"  />
             </td>
             <td>
                 <asp:ListBox ID="lstDestination" runat="server" Height="134px" Width="250px" SelectionMode="Multiple"></asp:ListBox></td>
--- validation/Controls/SelectTableList.ascx.cs
+++ validation/Controls/SelectTableList.ascx.cs
@@ -10,10 +10,25 @@ namespace validation.Controls
     public partial class SelectTableList : System.Web.UI.UserControl
     {
 
-        public string Title
-        {
+        public string Title {
             get { return lblTitle.Text; }
             set { lblTitle.Text = value; }
+        }
+        public string InitialItems
+        {
+            get
+            {
+                return lblTitle.Text;
+            }
+            set
+            {
+                string[] items = value.Split(',');
+                lstSource.Items.Clear();
+                foreach(String item in items)
+                {
+                    lstSource.Items.Add(new ListItem(item));
+                }
+            }
         }
 
         //1- Get a bunch of fruits to populate the source list view
@@ -22,7 +37,56 @@ namespace validation.Controls
         //3- Practice the validation controls for submitting the ADDRESS FORM
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void btnAllRight_Click(object sender, EventArgs e)
+        {
+            TransferAllItems(lstSource, lstDestination);
+        }
+
+        protected void btnSelectedRight_Click(object sender, EventArgs e)
+        {
+            TransferItems(lstSource, lstDestination);
+        }
+
+        protected void btnSelectedLeft_Click(object sender, EventArgs e)
+        {
+            TransferItems(lstDestination, lstSource);
+        }
+
+        protected void btnAllLeft_Click(object sender, EventArgs e)
+        {
+            TransferAllItems(lstDestination, lstSource);
+        }
+
+        private void TransferAllItems(ListBox lstFirst, ListBox lstSecond)
+        {
+            foreach (ListItem item in lstFirst.Items)
+            {
+                lstSecond.Items.Add(item);
+            }
+            lstFirst.Items.Clear();
+        }
+
+        private void TransferItems(ListBox lstFirst, ListBox lstSecond)
+        {
+            List<ListItem> ar1 = new List<ListItem>();
+            for (int i = 0; i < lstFirst.Items.Count; i++)
+            {
+                if (lstFirst.Items[i].Selected)
+                {
+                    if (!ar1.Contains(lstFirst.Items[i]))
+                        ar1.Add(lstFirst.Items[i]);
+                }
+            }
+            for (int i = 0; i < ar1.Count; i++)
+            {
+                if (!lstSecond.Items.Contains(ar1[i]))
+                    lstSecond.Items.Add(ar1[i]);
+                lstFirst.Items.Remove(ar1[i]);
+            }
+            lstSecond.SelectedIndex = -1;
         }
     }
 }
